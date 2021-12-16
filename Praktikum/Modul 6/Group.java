4f8ff12b91e971506cadb50f52f4b46866adbdd0
SonySantana154 <sony.santana@student.unjani.ac.id>
2021-12-16 17:53:48 +0700

    Praktikum modul 6

---
+++ Praktikum/Modul 6/Group.java
@@ -0,0 +1,11 @@
+public class Group {
+	public List getUsersSortedByMostRecentlyRegistered() {
+	List users = new ArrayList();
+		if (!userDirectoryExists())
+return users;
+
+	addFoundUsersTo(users);
+sortByMostRecentlyRegistered(users);
+
+return users;
+}
